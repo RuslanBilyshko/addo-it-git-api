bf09b113645cce299cf405b05e26cdda9608480c
ruslan <ruslan.ua@mail.ru>
2017-02-24 16:05:27 +0200

    Консоль для управления программой

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cmd
 
-from repository import Repository
 from argparser import ArgParser
+from repos.repository import Repository, Commit
 
 
 class Cli(cmd.Cmd):
@@ -12,29 +12,60 @@ class Cli(cmd.Cmd):
         self.doc_header = "Доступные команды (для справки по конкретной команде наберите 'help _команда_')"
 
     def do_repo(self, line: str):
-        """Получение списка репозиториев пользователя <repo -u [user_name]>"""
+        """Получение списка репозиториев пользователя. Нужно будет указать Имя пользователя"""
 
+        # """Получение списка репозиториев пользователя введите например: repo -u RuslanBilyshko"""
         # repo -u RuslanBilyshko
-        # test -u nesterenko-d
-        username = ArgParser.username(line)
-        repo = Repository(username)
-        #concrete_repository = repo.get("add-it-task2.2")
+        # repo -u nesterenko-d
 
-        print('Список репозиториев - ' + username)
+        # username = ArgParser.username(line)
+        username = input("Укажите имя пользователя: ")
+
+        repos = Repository(username).all().select(["name"])
+
+        print('Список репозиториев - {} ({})'.format(username, repos.count()))
         print("------------------------------------")
+        print(repos)
+
+    def do_test(self, line): pass
+
+    def do_stat(self, line):
+        """Статиcтика репозитория Нужно будет указать Имя пользователя и Репозитория"""
+
+        # """Статиcтика репозитория введите например: stat -u RuslanBilyshko -r add-it-task2.2"""
+
+        # stat -u RuslanBilyshko -r add-it-task2.2
+        # stat -u nesterenko-d -r rest-generator
+
+        # args = ArgParser.repos(line)
+        # rep = args["repos"]
+        # user = args["username"]
+
+        user = input("Укажите имя пользователя: ")
+        rep = input("Укажите имя репозитория: ")
 
 
+        commits_count = Commit(rep, user).all().count()
 
-        print(repo.all())
+        fields = [
+            "id",
+            "name",
+            "full_name",
+            "html_url",
+            "language",
+            "default_branch"
+        ]
 
-        # for field, value in concrete_repository.items():
-        #     print("{} : {}".format(field, value))
-        # for name in repo.get('name'):
-        #     print("repo: " + name)
+        repos = Repository(user).find(rep).select(fields)
 
+        print('Статистика репозитория - /{}/ (пользователь: {})'.format(rep, user))
+        print("------------------------------------")
+        print("Коммитов: {}".format(commits_count))
 
-    def do_stat(self, username: str, repos: str):
-        pass
+        # При таком варианте распечатки порядок полей конечно всегда разный
+        # Но ничего не стоит воспользоваться repos.get()
+        # И вывести сдесь все поля в нужном порядке
+        print(repos)
 
     def default(self, line):
         print("Несуществующая команда")
