248ff2cd8faecbdcc9022500554acc31417b9e87
ruslan <ruslan.ua@mail.ru>
2017-02-24 16:04:57 +0200

    Валидация параметров

--- argparser.py
+++ argparser.py
@@ -3,7 +3,15 @@ import argparse
 class ArgParser(object):
 
     @staticmethod
-    def username(line: str):
+    def username(line: str) -> str:
         parser = argparse.ArgumentParser()
         parser.add_argument('-u', '--username', required=True)
         return parser.parse_args(line.split()).username
+
+    @staticmethod
+    def repos(line: str) -> dict:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-r', '--repos', required=True)
+        parser.add_argument('-u', '--username', required=True)
+        result = parser.parse_args(line.split())
+        return {"username": result.username, "repos": result.repos }
